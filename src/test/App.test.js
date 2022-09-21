a3352a99bb33032e8953d516717e7bb4ffb19b27
tristan99 <ariescacy9@gmail.com>
2022-09-21 18:47:21 -0500

    eslint correct

--- src/test/App.test.js
+++ src/test/App.test.js
@@ -1,6 +1,7 @@
+/* global test, expect */
+import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from '../App';
-
 
 test('renders learn react link', () => {
   render(<App />);
